c6778eb95c4c6e55b4aba04c449c759d22627a89
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-02-27 17:48:07 +0800

    setup login form

---
+++ Login.elm
@@ -0,0 +1,66 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+type alias Model =
+    { username : String
+    , password : String
+    }
+
+
+type Msg
+    = UsernameInput String
+    | PasswordInput String
+
+
+initModel : Model
+initModel =
+    { username = ""
+    , password = ""
+    }
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        UsernameInput username ->
+            { model | username = username }
+
+        PasswordInput password ->
+            { model | password = password }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h3 [] [ text "Please Login" ]
+        , Html.form []
+            [ input
+                [ type_ "text"
+                , onInput UsernameInput
+                , placeholder "Enter username"
+                ]
+                []
+            , input
+                [ type_ "text"
+                , onInput PasswordInput
+                , placeholder "Enter password"
+                ]
+                []
+            , input
+                [ type_ "submit" ]
+                [ text "Submit" ]
+            ]
+        ]
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = initModel
+        , view = view
+        , update = update
+        }
