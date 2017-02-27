1356dba580ea57ef89c916c4ec136d112adba797
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-02-27 18:22:25 +0800

    setup leaderboard module

---
+++ LeaderBoard.elm
@@ -0,0 +1,60 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+type alias Model =
+    { runners : List Runner
+    , query : String
+    }
+
+
+type alias Runner =
+    { id : Int
+    , name : String
+    , location : String
+    }
+
+
+initModel : Model
+initModel =
+    { runners = []
+    , query = ""
+    }
+
+
+type Msg
+    = QueryInput String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        QueryInput query ->
+            { model | query = query }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text "Leader Board" ]
+        , Html.form []
+            [ input
+                [ type_ "text"
+                , onInput QueryInput
+                , placeholder "Search Here"
+                ]
+                []
+            ]
+        ]
+
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = initModel
+        , view = view
+        , update = update
+        }
