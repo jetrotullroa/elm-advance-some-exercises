fd023827cb780c79c1d2ccc1dffffb65473b8d1d
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-02-28 16:49:32 +0800

    decode json to string

---
+++ exercises/single-page-application/LeaderBoard.elm
@@ -0,0 +1,49 @@
+module LeaderBoard exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+type alias Model =
+    { runners : List Runner
+    , query : String
+    }
+
+
+type alias Runner =
+    { id : Int
+    , name : String
+    , location : String
+    }
+
+
+initModel : Model
+initModel =
+    { runners = []
+    , query = ""
+    }
+
+
+type Msg
+    = QueryInput String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        QueryInput query ->
+            { model | query = query }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text "Leader Board" ]
+        , input
+            [ type_ "text"
+            , onInput QueryInput
+            , placeholder "Search Here"
+            ]
+            []
+        ]
